cd23fc5cb112afbbdde67113280112405c88ea8c
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2026-02-09 23:03:12 -0300

    The final code I believe

--- CsharpProjects/TestProject3/Program.cs
+++ CsharpProjects/TestProject3/Program.cs
@@ -6,22 +6,20 @@ int roll3 = dice.Next(1, 7);
 
 int total = roll1 + roll2 + roll3;
 
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} +{roll3}");
-
-if(total > 14)
-{
-    Console.WriteLine("You win!");
-}
-
-if(total < 15)
-{
-    Console.WriteLine("Sorry, you lose.");
-}
-
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
 if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
     Console.WriteLine("You rolled doubles! +2 bonus to total!");
     total += 2;
+}
+
+if (total >= 15)
+{
+    Console.WriteLine("You win!");
+}
+
+if (total < 15)
+{
+    Console.WriteLine("Sorry, you lose.");
 }
