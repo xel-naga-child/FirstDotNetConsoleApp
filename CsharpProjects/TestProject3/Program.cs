a4aaf551d434547597cce8cb8aeb8b399d7d9552
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2026-02-09 23:09:18 -0300

    The final final else

--- CsharpProjects/TestProject3/Program.cs
+++ CsharpProjects/TestProject3/Program.cs
@@ -20,13 +20,23 @@ if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
         Console.WriteLine("You rolled doubles!  +2 bonus to total!");
         total += 2;
     }
+
+    Console.WriteLine($"Your total including the bonus: {total}");
 }
 
-if (total >= 15)
+if (total >= 16)
 {
-    Console.WriteLine("You win!");
+    Console.WriteLine("You win a new car!");
 }
-else 
+else if (total >= 10)
 {
-    Console.WriteLine("Sorry, you lose.");
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
 }
