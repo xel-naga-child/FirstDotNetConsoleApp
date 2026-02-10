8090427a968cbb2d7897a294c051439496ce6319
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2026-02-09 23:08:42 -0300

    The final about else

--- CsharpProjects/TestProject3/Program.cs
+++ CsharpProjects/TestProject3/Program.cs
@@ -10,14 +10,16 @@ Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
 if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-    Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-    total += 2;
-}
-
-if ((roll1 == roll2) && (roll2 == roll3))
-{
-    Console.WriteLine("You rolled triples!  +6 bonus to total!");
-    total += 6;
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
 }
 
 if (total >= 15)
