e4ba512f595afcdcbcaf5e50cd75f65b80cac5b3
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2026-02-10 23:37:25 -0300

    Not my first reassign

--- CsharpProjects/TestProject5/Program.cs
+++ CsharpProjects/TestProject5/Program.cs
@@ -8,3 +8,7 @@ fraudulentOrderIDs[2] = "C789";
 Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
 Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
 Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
