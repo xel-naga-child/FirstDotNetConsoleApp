c52824c381f2785e85fd198f5a298e1f7d26c55e
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2026-02-10 23:36:09 -0300

    Not my first array print

--- CsharpProjects/TestProject5/Program.cs
+++ CsharpProjects/TestProject5/Program.cs
@@ -3,4 +3,8 @@
 fraudulentOrderIDs[0] = "A123";
 fraudulentOrderIDs[1] = "B456";
 fraudulentOrderIDs[2] = "C789";
-fraudulentOrderIDs[3] = "D000";
+// fraudulentOrderIDs[3] = "D000";
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
