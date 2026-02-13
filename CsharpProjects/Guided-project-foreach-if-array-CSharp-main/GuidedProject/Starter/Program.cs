3a185c37bd6b9a7ebec93a49160e16333fc2cb52
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2026-02-12 22:54:32 -0300

    all in an array (NOT MY FIRST

--- CsharpProjects/Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
+++ CsharpProjects/Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
@@ -2,30 +2,10 @@
 
 // initialize variables - graded assignments 
 int currentAssignments = 5;
-
-int sophia1 = 90;
-int sophia2 = 86;
-int sophia3 = 87;
-int sophia4 = 98;
-int sophia5 = 100;
-
-int andrew1 = 92;
-int andrew2 = 89;
-int andrew3 = 81;
-int andrew4 = 96;
-int andrew5 = 90;
-
-int emma1 = 90;
-int emma2 = 85;
-int emma3 = 87;
-int emma4 = 98;
-int emma5 = 68;
-
-int logan1 = 90;
-int logan2 = 95;
-int logan3 = 87;
-int logan4 = 88;
-int logan5 = 96;
+int [] sophiaScores = new int[] {90, 86, 87, 98, 100};
+int [] andrewScores = new int[] {90, 89, 81, 96, 90};
+int [] emmaScores = new int[] {90, 85, 87, 98, 68};
+int [] loganScores = new int[] {90, 95, 87, 88, 96};
 
 int sophiaSum = 0;
 int andrewSum = 0;
