348bc700481492b307b92d73c3a037404f088f62
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2026-02-12 23:00:16 -0300

    Sophia left alone

--- CsharpProjects/Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
+++ CsharpProjects/Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
@@ -8,30 +8,18 @@ int [] emmaScores = new int[] {90, 85, 87, 98, 68};
 int [] loganScores = new int[] {90, 95, 87, 88, 96};
 
 int sophiaSum = 0;
-int andrewSum = 0;
-int emmaSum = 0;
-int loganSum = 0;
 
 decimal sophiaScore;
-decimal andrewScore;
-decimal emmaScore;
-decimal loganScore;
 
-sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
-andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
-emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
-loganSum = logan1 + logan2 + logan3 + logan4 + logan5;
+foreach (int score in sophiaScores)
+{
+    sophiaSum += score;    
+}
 
 sophiaScore = (decimal)sophiaSum / currentAssignments;
-andrewScore = (decimal)andrewSum / currentAssignments;
-emmaScore = (decimal)emmaSum / currentAssignments;
-loganScore = (decimal)loganSum / currentAssignments;
 
 Console.WriteLine("Student\t\tGrade\n");
 Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
-Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
-Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
-Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
