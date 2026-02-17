282a4a390da9c4630ef06da6160f8dfe33183367
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2026-02-16 22:30:15 -0300

    Not my first variable init

--- CsharpProjects/TestProject7/Program.cs
+++ CsharpProjects/TestProject7/Program.cs
@@ -1,8 +1,10 @@
 ﻿bool flag = true;
+int value = 0;
+
 if (flag)
 {
-    int value = 10;
     Console.WriteLine($"Inside the code block: {value}");
 }
 
+value = 10;
 Console.WriteLine($"Outside the code block: {value}");
