13f34f9ddbcea4d5369f8d1873a7c8aad059929f
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2026-02-16 22:37:17 -0300

    another exercise

--- CsharpProjects/TestProject7/Program.cs
+++ CsharpProjects/TestProject7/Program.cs
@@ -1,2 +1,4 @@
-﻿bool flag = true;
-if (flag) Console.WriteLine(flag);
+﻿string name = "steve";
+if (name == "bob") Console.WriteLine("Found Bob");
+else if (name == "steve") Console.WriteLine("Found Steve");
+else Console.WriteLine("Found Chuck");
