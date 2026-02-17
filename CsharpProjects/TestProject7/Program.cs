460495c7b2670b3d6d37429ed8b7df03114af38e
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2026-02-16 22:34:33 -0300

    Boolean print

--- CsharpProjects/TestProject7/Program.cs
+++ CsharpProjects/TestProject7/Program.cs
@@ -1,10 +1,5 @@
 ﻿bool flag = true;
-int value = 0;
-
 if (flag)
 {
-    Console.WriteLine($"Inside the code block: {value}");
+    Console.WriteLine(flag);
 }
-
-value = 10;
-Console.WriteLine($"Outside the code block: {value}");
