ebed423f2d16624be06f84a874209edebc4de7fe
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2026-02-17 22:22:14 -0300

    Not my first denie

--- CsharpProjects/TestProject8/Program.cs
+++ CsharpProjects/TestProject8/Program.cs
@@ -1,3 +1,3 @@
 ﻿string pangram = "The quick brown fox jumps over the lazy dog.";
-Console.WriteLine(pangram.Contains("fox"));
-Console.WriteLine(pangram.Contains("cow"));
+Console.WriteLine(!pangram.Contains("fox"));
+Console.WriteLine(!pangram.Contains("cow"));
