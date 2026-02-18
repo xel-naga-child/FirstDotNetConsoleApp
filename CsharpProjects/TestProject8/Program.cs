075f78e19d9529d4be240ddc3092b61ce9d23b66
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2026-02-17 22:19:46 -0300

    Not my first using bool method

--- CsharpProjects/TestProject8/Program.cs
+++ CsharpProjects/TestProject8/Program.cs
@@ -1,4 +1,3 @@
-﻿Console.WriteLine(1 > 2);
-Console.WriteLine(1 < 2);
-Console.WriteLine(1 >= 1);
-Console.WriteLine(1 <= 1);
+﻿string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(pangram.Contains("fox"));
+Console.WriteLine(pangram.Contains("cow"));
