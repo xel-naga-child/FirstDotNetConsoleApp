5717420404e62107c14bc1e1cd37cf5ce8654511
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2026-02-17 22:18:14 -0300

    Not my first unequality print

--- CsharpProjects/TestProject8/Program.cs
+++ CsharpProjects/TestProject8/Program.cs
@@ -1,3 +1,6 @@
-﻿string value1 = " a";
-string value2 = "A ";
-Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+﻿Console.WriteLine("a" != "a");
+Console.WriteLine("a" != "A");
+Console.WriteLine(1 != 2);
+
+string myValue = "a";
+Console.WriteLine(myValue != "a");
