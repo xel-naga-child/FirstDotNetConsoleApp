a0e03f7c7cc46386c60843cb0fc0607043ea465c
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2026-02-17 22:25:56 -0300

    I already knew that

--- CsharpProjects/TestProject8/Program.cs
+++ CsharpProjects/TestProject8/Program.cs
@@ -1,3 +1,6 @@
-﻿string pangram = "The quick brown fox jumps over the lazy dog.";
-Console.WriteLine(!pangram.Contains("fox"));
-Console.WriteLine(!pangram.Contains("cow"));
+﻿int a = 7;
+int b = 6;
+Console.WriteLine(a != b); // output: True
+string s1 = "Hello";
+string s2 = "Hello";
+Console.WriteLine(s1 != s2); // output: False
