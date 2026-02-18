9ed6a8dc79004bd9f8e8b8b4ec3ba310a81d51a8
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2026-02-17 22:17:23 -0300

    Not my strig equality

--- CsharpProjects/TestProject8/Program.cs
+++ CsharpProjects/TestProject8/Program.cs
@@ -1,6 +1,3 @@
-﻿Console.WriteLine("a" == "a");
-Console.WriteLine("a" == "A");
-Console.WriteLine(1 == 2);
-
-string myValue = "a";
-Console.WriteLine(myValue == "a");
+﻿string value1 = " a";
+string value2 = "A ";
+Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
