0723a8eaf4913e3121ba369682a9e24d46b77dd7
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2026-02-17 22:18:42 -0300

    Not my first comparision

--- CsharpProjects/TestProject8/Program.cs
+++ CsharpProjects/TestProject8/Program.cs
@@ -1,6 +1,4 @@
-﻿Console.WriteLine("a" != "a");
-Console.WriteLine("a" != "A");
-Console.WriteLine(1 != 2);
-
-string myValue = "a";
-Console.WriteLine(myValue != "a");
+﻿Console.WriteLine(1 > 2);
+Console.WriteLine(1 < 2);
+Console.WriteLine(1 >= 1);
+Console.WriteLine(1 <= 1);
