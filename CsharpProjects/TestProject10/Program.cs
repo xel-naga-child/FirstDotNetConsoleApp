6f03960a44d8100d70b6ed90fd67ea11dcc313c1
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2026-02-21 02:56:11 -0300

    This is better

--- CsharpProjects/TestProject10/Program.cs
+++ CsharpProjects/TestProject10/Program.cs
@@ -1,6 +1,5 @@
 ﻿string[] names = { "Alex", "Eddie", "David", "Michael" };
-foreach (var name in names)
-{
-    // Can't do this:
-    if (name == "David") name = "Sammy";
-}
+for (int i = 0; i < names.Length; i++)
+    if (names[i] == "David") names[i] = "Sammy";
+
+foreach (var name in names) Console.WriteLine(name);
