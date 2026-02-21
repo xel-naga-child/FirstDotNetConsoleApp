db0671fa3c7aef3c5c678c71ae9aa2aed98d5726
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2026-02-21 03:07:52 -0300

    Mine is better

--- CsharpProjects/TestProject10/Program.cs
+++ CsharpProjects/TestProject10/Program.cs
@@ -1,8 +1,11 @@
-﻿for (int i = 1; i <= 100; i++)
+﻿for (int i = 1; i < 101; i++)
 {
-    if (i % 3 == 0 && i % 5 == 0) Console.WriteLine($"FizzBuzz - {i}");
-    else if (i % 3 == 0) Console.WriteLine($"Fizz - {i}");
-    else if (i % 5 == 0) Console.WriteLine($"Buzz - {i}");
-    else Console.WriteLine(i);
-
+    if ((i % 3 == 0) && (i % 5 == 0))
+        Console.WriteLine($"{i} - FizzBuzz");
+    else if (i % 3 == 0)
+        Console.WriteLine($"{i} - Fizz");
+    else if (i % 5 == 0)
+        Console.WriteLine($"{i} - Buzz");
+    else
+        Console.WriteLine($"{i}");
 }
