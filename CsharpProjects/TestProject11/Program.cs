cbc2083aa8f287f6fa95b66b4a9da54e666044bc
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2026-03-07 03:42:42 -0300

    Not my first while

--- CsharpProjects/TestProject11/Program.cs
+++ CsharpProjects/TestProject11/Program.cs
@@ -1,8 +1,17 @@
 ﻿Random random = new Random();
-int current = 0;
+int current = random.Next(1, 11);
 
+/*
 do
 {
     current = random.Next(1, 11);
     Console.WriteLine(current);
 } while (current != 7);
+*/
+
+while (current >= 3)
+{
+    Console.WriteLine(current);
+    current = random.Next(1, 11);
+}
+Console.WriteLine($"Last number: {current}");
